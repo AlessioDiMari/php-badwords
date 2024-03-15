25c979a47c8aa22e7a7ec7f738e90e8bb00cc637
AlessioDiMari <alessiodimari.adm25@gmail.com>
2024-03-15 19:29:57 +0100

    fine

--- result.php
+++ result.php
@@ -1,6 +1,7 @@
 <?php
 
 $badword = $_GET['user-badword'];
+$paragraph = $_GET['user-paragraph'];
 
 ?>
 
@@ -9,12 +10,39 @@ $badword = $_GET['user-badword'];
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="style.css">
     <title>Risultato</title>
 </head>
 <body>
 
-    <h1>Ecco il risultato</h1>
-    <p>La parola che hai deciso di censurare è: <?php echo $badword ?></p>
+    <div class="container">
+        <h1>Ecco il risultato</h1>
+        <p>La parola che hai deciso di censurare è: <?php echo $badword ?></p>
+        
+        <div class="paragraph">
+            Il tuo paragrafo è:
+            <p>
+                <?php
+                echo $paragraph
+                ?>
+                </p>
+            <div class="lenght">
+                Lunghezza: <?php echo strlen($paragraph)?>
+            </div>
+        </div>
+        <div class="cens-paragraph">
+            Il tuo paragrafo censurato è:
+            <p>
+                <?php
+                $censParagraph = str_ireplace($badword,"***",$paragraph);
+                echo $censParagraph
+                ?>
+            </p>
+            <div class="lenght">
+                Lunghezza: <?php echo strlen($censParagraph)?>
+            </div>
+        </div>
+    </div>
 
 </body>
 </html>

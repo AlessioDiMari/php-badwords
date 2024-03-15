d6f1e7a2a904eb1c2a6ae984ad5ab5f083f91b19
AlessioDiMari <alessiodimari.adm25@gmail.com>
2024-03-15 17:33:01 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="it">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Badwords</title>
+</head>
+<body>
+
+    <h1>ciao</h1>
+
+    <?php
+
+    $vari = "Alessio";
+    echo "Ciao sono $vari." 
+    ?>
+    
+
+</body>
+</html>
